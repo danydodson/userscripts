8e7f19cfb7fd4281e772e355420b1e8d8042941d
danydodson <danydodson@gmail.com>
2023-06-21 07:55:36 -0500

    :sparkles: updates

---
+++ src/fidelity/Fidelity-Cleanup.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Fidelity: Cleanup
+// @namespace    https://github.com/danydodson/userscripts/blob/main/src/fidelity/Fidelity-Cleanup.user.js
+// @downloadURL  https://github.com/danydodson/userscripts/raw/main/src/fidelity/Fidelity-Cleanup.user.js
+// @updateURL    https://github.com/danydodson/userscripts/raw/main/src/fidelity/Fidelity-Cleanup.user.js
+// @version      0.1
+// @description  removes unnecessary elements from fidelity ui
+// @author       toaster
+// @match        https://digital.fidelity.com/*
+// @icon         https://digital.fidelity.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	'use strict'
+
+	function clearFeedbackBtn() {
+		try {
+			let feedbackBtn = document.getElementsByClassName('SI_0AsPpi6JZXIjgMZ_FeedBackLinkContainer')
+			if (feedbackBtn) feedbackBtn.style.display = 'none'
+		} catch (exception) {
+			console.error(exception)
+		}
+	}
+
+	clearFeedbackBtn()
+
+})()
