c602c47a06d1d17f250f3bc88b645d359af10a0a
danydodson <danydodson@gmail.com>
2023-08-15 03:36:10 -0500

    ✨

--- src/template/template.user.js
+++ src/template/template.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name        template
-// @namespace   dany
 // @description template
 // @version     1.0.0
 // @author      dany
+// @namespace   https://github.com/danydodson/userscripts
 // @license     MIT
 // @icon        https:danydodson.dev/favicon.ico
 // @downloadURL https://github.com/danydodson/userscripts/raw/main/src/templatetemplate.user.js
+// @updateURL   https://github.com/danydodson/userscripts/raw/main/src/template/template.user.js
 // @homepageURL https://github.com/danydodson/userscripts
 // @supportURL  https://github.com/danydodson/userscripts/issues
-// @updateURL   https://github.com/danydodson/userscripts/raw/main/src/template/template.user.js
 // @grant       none
 // @match       *://*/*
 // ==/UserScript==
