f1b605eb6f8efcd2b68e87af4288b8aa38ec76fc
dany <danydodson@gmail.com>
2024-10-20 19:35:00 -0500

    Add files via upload

--- src/Twitch_Ads_Solutions.user.js
+++ src/Twitch_Ads_Solutions.user.js
@@ -6,8 +6,8 @@
 // @description  Multiple solutions for blocking Twitch ads (vaft)
 // @namespace    dumpsterbaby.lol
 // @homepage     https://github.com/danydodson/userscripts/blob/main/src/Twitch_Ads_Solutions.user.js
-// @updateURL    https://github.com/danydodson/userscripts/blob/main/src/Twitch_Ads_Solutions.user.js
-// @downloadURL  https://github.com/danydodson/userscripts/blob/main/src/Twitch_Ads_Solutions.user.js
+// @updateURL    https://github.com/danydodson/userscripts/raw/refs/heads/main/src/Twitch_Ads_Solutions.user.js
+// @downloadURL  https://github.com/danydodson/userscripts/raw/refs/heads/main/src/Twitch_Ads_Solutions.user.js
 // @icon         https://icons.duckduckgo.com/ip2/twitch.tv.ico
 // @match        *://*.twitch.tv/*
 // @run-at       document-start
