fb856d7c42f74fe52ad9d9eaa8ad99e505e3f5c2
danydodson <danydodson@gmail.com>
2025-09-01 20:55:51 -0500

    refactor and clean

--- src/m3u8_detector.user.js
+++ src/m3u8_detector.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         M3U8 Detector
+// @name         M3U8 Detect and Download
 // @version      1.4.1
 // @description  Automatically detect the m3u8 video of the page and download it completely. Once detected the m3u8 link, it will appear in the upper right corner of the page. Click download to jump to the m3u8 downloader.
 // @icon         https://tools.thatwind.com/favicon.png
@@ -365,10 +365,12 @@
   const barBtn = bar.querySelector(".number-indicator");
 
   // About visibility and movement
+
   (async function () {
 
     let shown = await GM_getValue("shown", true);
     wrapper.setAttribute("data-shown", shown);
+
 
     let x = await GM_getValue("x", 10);
     let y = await GM_getValue("y", 10);
@@ -496,6 +498,7 @@
         );
       }
     });
+
   }
 
 
